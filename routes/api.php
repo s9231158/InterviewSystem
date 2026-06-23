4b74280fa1bae72a89d344e9fd0c4cc7fe2fe016
s9231158 <s9231158@gmail.com>
2026-06-23 14:00:39 +0800

    databaseTest

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::get('/test', function () {
+    $user = App\Models\User::first();
     return response()->json([
-        'message' => 'Test API is working!',
+        'message' => $user,
     ]);
 });
