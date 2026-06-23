3cc209cf300778621d56f9ce05ac14ae0c277bd8
s9231158 <s9231158@gmail.com>
2026-06-23 12:23:14 +0800

    Ensure file ownership is nginx inside runtime directory after backup restore

--- .docker/run.sh
+++ .docker/run.sh
@@ -3,6 +3,11 @@
 echo "Restoring application files from backup..."
 cp -R /var/www/html_backup/. /var/www/html/
 
+# Ensure web server has write permissions to storage and cache
+echo "Setting folder permissions..."
+chown -R nginx:nginx /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 echo "Caching config..."
 php artisan config:cache
 
